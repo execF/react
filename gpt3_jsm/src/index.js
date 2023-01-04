a859ff4f619b0cb75232106bbeb5b19216832898
iCloudTrungTM <tranminh.trung@icloud.com>
2023-01-04 13:55:47 +0100

    gpt3_jsm

--- gpt3_jsm/src/index.js
+++ gpt3_jsm/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css'
 
-ReactDOM.redner(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
+
